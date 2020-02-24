902a383bb74728cf02fc2e7a08bdb14e9f9de13f
Marco Schroeder <marcoschr@gmx.de>
2020-02-24 22:20:45 +0100

    add circle plot

--- src/oloid/circle.py
+++ src/oloid/circle.py
@@ -1,11 +1,23 @@
 import numpy as np
+from matplotlib.lines import Line2D
+from mpl_toolkits.mplot3d import proj3d
+from mpl_toolkits.mplot3d.art3d import Line3D
 
-class circle:
+class circle(Line3D):
     def __init__(self,center_point, normal_vector, radius=1,nr_of_points=100):
         self.center_point = center_point
         self.normal_vector = normal_vector
         self.radius = radius
         self.nr_of_points = nr_of_points
+        Line3D.__init__(self, [], [], [], label='parametric curve')
+
+
+    def draw(self, renderer):
+        x,y,z = self.calculate()
+        xs3d, ys3d, zs3d = x,y,z
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_data_3d(x, y,z)
+        Line3D.draw(self, renderer)
 
     def get_x(self):
         x,y,z = self.calculate()
---
+++ src/oloid/plot_fkt.py
@@ -0,0 +1,54 @@
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D, proj3d
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+
+import oloid.circle
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+# #dibujar cubo
+r = [-1, 1]
+for s, e in itertools.combinations(np.array(list(itertools.product(r,r,r))), 2):
+    if np.sum(np.abs(s-e)) == r[1]-r[0]:
+        ax.plot3D(*zip(s,e), color="b")
+
+#dibujar punto
+ax.scatter([0],[0],[0],color="g",s=100)
+
+
+class Arrow3D(mpatches.FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        mpatches.FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        mpatches.FancyArrowPatch.draw(self, renderer)
+
+
+#m=float(raw_input())
+a = Arrow3D([0,0],[0,1],[0,0], mutation_scale=20, lw=1, arrowstyle="-|>", color="k")
+b = Arrow3D([0,-1],[0,0],[0,0], mutation_scale=20, lw=1, arrowstyle="-|>", color="r")
+c = Arrow3D([0,0],[0,0],[0,1], mutation_scale=20, lw=1, arrowstyle="-|>", color="b")
+d = Arrow3D([0,0],[0,0],[0,-1], mutation_scale=20, lw=1, arrowstyle="-|>", color="g")
+e = Arrow3D([0,1],[0,0],[0,0], mutation_scale=20, lw=1, arrowstyle="-|>", color="c")
+f = Arrow3D([0,0],[0,-0.5],[0,0], mutation_scale=20, lw=1, arrowstyle="-|>", color="m")
+
+my_circle = oloid.circle.circle([0, 0, 0], [0, 0, 0], 1, 100)
+
+ax.add_artist(my_circle)
+ax.add_artist(a)
+ax.add_artist(b)
+ax.add_artist(c)
+ax.add_artist(d)
+ax.add_artist(e)
+ax.add_artist(f)
+ax.legend()
+plt.show()
